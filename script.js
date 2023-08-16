0a687d75023df2fe9769db8263cb3f4d2fd9c68d
shraddha2903 <70905602+shraddha2903@users.noreply.github.com>
2023-08-16 16:42:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 function calculateMinCost() {
   //your code here
-  
-  
-  
+	var x = document.getElementById("rope-lengths").value;
+	
+  var input= x.split(",");
+
+  // let n=input.length;
+	var ans=0;
+  while(input.length>1)
+	  {
+		  input.sort();
+		  let sum=0;
+		   sum=parseInt(input[0])+parseInt(input[1]);
+		  document.getElementById('intr').innerHTML=sum;
+		  
+		  ans=parseInt(ans)+sum;
+		  console.log(ans);
+		  input.splice(0,2,sum);
+		  break;
+	  }
+	document.getElementById('result').innerHTML=ans;
 }  
